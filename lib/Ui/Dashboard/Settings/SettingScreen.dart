5fc495fe09d9371d8cb8cfd8736af4c7a3f531ce
coderwork23 <austin.tech821@gmail.com>
2023-11-28 18:49:44 +0530

    version update

--- lib/Ui/Dashboard/Settings/SettingScreen.dart
+++ lib/Ui/Dashboard/Settings/SettingScreen.dart
@@ -278,7 +278,7 @@ class _SettingScreenState extends State<SettingScreen> {
                   const SizedBox(width: 10),
 
                   Text(
-                    "1.0.0",
+                    "4.4.0",
                     style:MyStyle.tx18RWhite.copyWith(
                         fontSize: 18,
                         color: MyColor.grey01Color
